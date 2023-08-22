86c0cce512c50650d76460d5acb8f1e0aa95d94d
Tuguldur Gantumur <gtrtuugii@gmail.com>
2023-08-22 17:28:19 +1000

    updated post time
    Changed the format similar to other social media pages "1h ago" etc.

--- src/components/dashboard/Post.jsx
+++ src/components/dashboard/Post.jsx
@@ -7,6 +7,30 @@ const Post = ({post}) => {
     const {currentUser} =  useContext(AuthContext);
     const {data} =  useContext(ChatContext);
     const ref = useRef();
+
+    // Function to format time difference
+function formatTimeDifference(date) {
+  const now = new Date();
+  const diff = Math.floor((now - date) / 1000); // Difference in seconds
+
+  if (diff < 60) {
+    return `${diff} seconds ago`;
+  } else if (diff < 3600) {
+    const minutes = Math.floor(diff / 60);
+    return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+  } else if (diff < 86400) {
+    const hours = Math.floor(diff / 3600);
+    return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+  } else if (diff < 604800) {
+    const days = Math.floor(diff / 86400);
+    return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+  } 
+   else {
+    // Display full date without the year
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  }
+}
   
     if(!post){
       return null;
@@ -17,9 +41,14 @@ const Post = ({post}) => {
         <div className="card">
               <div className="card-header">
                 <img className="user-profile-pic" src={post?.publisherDisplayImg} alt='' />
-                <span>{" #" + post?.publisherDisplayName}</span>
                 
+                <div className="user">
+                  <span>{" #" + post?.publisherDisplayName}</span>
+                  <span id="date">{formatTimeDifference(post.date?.toDate())}</span>
+
+                </div>
               </div>
+              
               <div className="card-body">
                 
                 {post.title && <h4>{post?.title}</h4>}
@@ -28,7 +57,8 @@ const Post = ({post}) => {
               </div>
               
               <div className="card-footer">
-                <span> {post.date?.toDate().toLocaleDateString() + ' ' + post.date?.toDate().toLocaleTimeString()}</span>
+                <button className='btn-dark'>Like</button>
+                <button className='btn-dark'>Comment</button>
               </div>
             </div>
     </div>
