e60e696e3b72afd17dc4f43f86585a8b758e1a51
Tuguldur Gantumur <gtrtuugii@gmail.com>
2023-08-26 04:11:16 +1000

    fixed meme creator
    can now generate a meme without downloading it (bad experience on IOS)

--- src/components/meme/meme.jsx
+++ src/components/meme/meme.jsx
@@ -10,43 +10,82 @@ const MemeCreator = () => {
     const [fontSize, setFontSize] = useState(36); // Default font size
     const [fontFamily, setFontFamily] = useState('Impact'); // Default font family
     const [fontWeight, setFontWeight] = useState('normal'); // Default font weight
+
+    const [finalMemeImage, setFinalMemeImage] = useState(null);
+    const [memeCreated, setMemeCreated] = useState(false);
   
     const handleImageUpload = (event) => {
       const uploadedImage = event.target.files[0];
       const imageUrl = URL.createObjectURL(uploadedImage);
       setImage(imageUrl);
     };
-  
+
+    const handleMeme = () => {
+        const canvas = document.createElement('canvas');
+        const img = new Image();
+    
+        img.src = image;
+    
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext('2d');
+    
+            // Draw the image
+            ctx.drawImage(img, 0, 0);
+    
+            // Draw top text
+            ctx.fillStyle = fontColor;
+            ctx.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
+            ctx.textAlign = 'center';
+            ctx.fillText(topText, canvas.width / 2, canvas.height * 0.15);
+    
+            // Draw bottom text
+            ctx.fillText(bottomText, canvas.width / 2, canvas.height * 0.95);
+    
+            // Convert canvas to image URL
+            const memeImage = canvas.toDataURL('image/png');
+            setFinalMemeImage(memeImage);
+            setMemeCreated(true);
+        };
+    };
+    
     const handleDownload = () => {
-      const canvas = document.createElement('canvas');
-      const img = new Image();
+        const canvas = document.createElement('canvas');
+        const img = new Image();
   
-      img.src = image;
-  
-      img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        const ctx = canvas.getContext('2d');
-  
-        // Draw the image
-        ctx.drawImage(img, 0, 0);
-  
-        // Draw top text
-        ctx.fillStyle = fontColor;
-        ctx.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
-        ctx.textAlign = 'center';
-        ctx.fillText(topText, canvas.width / 2, 40);
-  
-        // Draw bottom text
-        ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
-  
-        // Convert canvas to image URL and initiate download
-        const memeImage = canvas.toDataURL('image/png');
-        const downloadLink = document.createElement('a');
-        downloadLink.href = memeImage;
-        downloadLink.download = 'meme.png';
-        downloadLink.click();
-      };
+        img.src = image;
+    
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext('2d');
+    
+            // Draw the image
+            ctx.drawImage(img, 0, 0);
+    
+            // Draw top text
+            ctx.fillStyle = fontColor;
+            ctx.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
+            ctx.textAlign = 'center';
+
+            ctx.fillText(topText, canvas.width / 2, canvas.height * 0.15);
+    
+            // Draw bottom text
+            ctx.fillText(bottomText, canvas.width / 2, canvas.height * 0.95);
+            
+
+            // Convert canvas to image URL and initiate download
+            const memeImage = canvas.toDataURL('image/png');
+            const downloadLink = document.createElement('a');
+            // Display the meme image
+            setMemeCreated(true);
+            setFinalMemeImage(memeImage);
+
+            downloadLink.href = memeImage;
+            downloadLink.download = 'meme.png';
+            downloadLink.click();
+        };
     };
   return (
     <div className='meme'>
@@ -100,11 +139,24 @@ const MemeCreator = () => {
                             value={fontFamily}
                             onChange={(e) => setFontFamily(e.target.value)}
                         >
-                        <option value="Impact">Impact</option>
-                        <option value="Rubik">Rubik</option>
-                        <option value="Press Start 2P">Press Start 2P</option>
                         <option value="Arial">Arial</option>
                         <option value="Times New Roman">Times New Roman</option>
+                        <option value="Impact">Impact</option>
+                        <option value="Georgia">Georgia</option>
+                        <option value="Verdana">Verdana</option>
+                        <option value="Brush Script MT">Brush Script MT</option>
+                        <option value="Courier New">Courier New</option>
+                        <option value="Trebuchet MS">Trebuchet MS</option>
+                        <option value="Rubik">Rubik</option>
+                        <option value="Press Start 2P">Press Start 2P</option>
+                        <option value="Garamond">Garamond</option>
+                        <option value="Copperplate">Copperplate</option>
+                        <option value="Papyrus">Papyrus</option>
+                        
+                        
+
+                        
+                        
                         {/* Add more font options here */}
                         </select>
                     </label>
@@ -121,12 +173,16 @@ const MemeCreator = () => {
                     {/* Add more font weight options here */}
                     </select>
                     </label>
+                    <p>Font "Press Start 2P" cannot be enlarged</p>
                 </div>
+                
                 <div className="img-preview">
-                    {image && <img src={image} alt="Uploaded" />}
+                    {finalMemeImage && <img src={finalMemeImage} alt="Meme" />}
                 </div>
                 
                 <div className="card-footer">
+                    <p>Mobile device users just press hold on the image to download</p>
+                    <button className="btn-light" onClick={handleMeme}>Create Meme</button>
                     <button className="btn-light" onClick={handleDownload}>Download Meme</button>
                 </div>
                 
